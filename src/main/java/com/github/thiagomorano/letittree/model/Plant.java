1707de9adc0c47a3b0d8629f7661b8a2b59e86b0
Thiago Morano <thiago@flying-sheep.com>
2023-10-06 17:29:44 +0200

    extend getAllPlants test; add id to plants

--- src/main/java/com/github/thiagomorano/letittree/model/Plant.java
+++ src/main/java/com/github/thiagomorano/letittree/model/Plant.java
@@ -1,4 +1,13 @@
 package com.github.thiagomorano.letittree.model;
 
 public class Plant {
+	private Long id;
+
+	public Plant(Long id) {
+		this.id = id;
+	}
+
+	public Long getId() {
+		return id;
+	}
 }
--- src/test/java/com/github/thiagomorano/letittree/LetItTreeServiceTest.java
+++ src/test/java/com/github/thiagomorano/letittree/LetItTreeServiceTest.java
@@ -1,11 +1,15 @@
 package com.github.thiagomorano.letittree;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.when;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
+import org.assertj.core.util.Arrays;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -29,7 +33,22 @@ public class LetItTreeServiceTest {
 	public void givenEmptyRepository_whenGetAllPlants_thenReturnsEmptyArray() {
 		when(mockPlantRepository.findAllPlants()).thenReturn(new ArrayList<>());
 
-		List<Plant> response = letItTreeService.getAllPlants();
-		assertEquals(0, response.size());
+		List<Plant> result = letItTreeService.getAllPlants();
+		assertEquals(0, result.size());
+	}
+
+	@Test
+	public void givenRepository_whenGetAllPlants_thenReturnsExistingPlants() {
+		Plant plant1 = new Plant(0L);
+		Plant plant2 = new Plant(1L);
+		List<Plant> plants = List.of(plant1, plant2);
+
+		when(mockPlantRepository.findAllPlants()).thenReturn(plants);
+
+		List<Plant> result = letItTreeService.getAllPlants();
+		assertEquals(2, result.size());
+		Set<Plant> expectedSet = new HashSet<>(plants);
+		Set<Plant> resultSet = new HashSet<>(result);
+		assertEquals(expectedSet, resultSet);
 	}
 }
