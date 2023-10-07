4274408b3564b58e3e99bfb4b14f4fcd25d53a2d
Thiago Morano <thiago@flying-sheep.com>
2023-10-07 15:32:15 +0200

    add fake repository

---
+++ src/main/java/com/github/thiagomorano/letittree/repository/FakePlantRepository.java
@@ -0,0 +1,28 @@
+package com.github.thiagomorano.letittree.repository;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.stereotype.Repository;
+
+import com.github.thiagomorano.letittree.model.Plant;
+
+@Repository
+public class FakePlantRepository implements PlantRepository {
+	private List<Plant> plantDatabase = new ArrayList<>();
+
+	@Override
+	public List<Plant> findAllPlants() {
+		return plantDatabase;
+	}
+
+	@Override
+	public Optional<Plant> findById(Long id) {
+		Optional<Plant> result = plantDatabase.stream()
+				.filter(plant -> plant.getId().equals(id))
+				.findFirst();
+		return result;
+	}
+
+}
